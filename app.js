83c353f2af070b51872c1d41309607785a917d00
Victor Kane <victorkane@gmail.com>
2020-08-11 07:48:27 -0400

    feat(content model): add poem piece type module, together with poem pages and poem widgets modules, giving options to create poems, and to create a Poems Index page

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-var path = require('path');
+var path = require("path");
 
-var apos = require('apostrophe')({
-  shortName: 'bare-bones',
+var apos = require("apostrophe")({
+	shortName: "bare-bones",
 
-  // See lib/modules for basic project-level configuration of our modules
-  // responsible for serving static assets, managing page templates and
-  // configuring user accounts.
+	// See lib/modules for basic project-level configuration of our modules
+	// responsible for serving static assets, managing page templates and
+	// configuring user accounts.
 
-  modules: {
+	modules: {
+		// Apostrophe module configuration
 
-    // Apostrophe module configuration
+		// Note: most configuration occurs in the respective
+		// modules' directories. See lib/apostrophe-assets/index.js for an example.
 
-    // Note: most configuration occurs in the respective
-    // modules' directories. See lib/apostrophe-assets/index.js for an example.
-    
-    // However any modules that are not present by default in Apostrophe must at
-    // least have a minimal configuration here: `moduleName: {}`
+		// However any modules that are not present by default in Apostrophe must at
+		// least have a minimal configuration here: `moduleName: {}`
 
-    // If a template is not found somewhere else, serve it from the top-level
-    // `views/` folder of the project
+		// If a template is not found somewhere else, serve it from the top-level
+		// `views/` folder of the project
 
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
+		"apostrophe-templates": {
+			viewsFolderFallback: path.join(__dirname, "views")
+		},
 
-  }
+		// piece types
+		poems: {},
+		"poems-pages": {},
+		"poems-widgets": {}
+	}
 });
