a5afba40184a7228844248804d3137e9eed3e110
Victor Kane <victorkane@gmail.com>
2020-08-14 15:32:01 -0400

    feat(people page]: Create people-pages module and register page type with apostrophe-pages

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
 var path = require("path");
 
 var apos = require("apostrophe")({
-  shortName: "bare-bones",
+	shortName: "bare-bones",
 
-  // See lib/modules for basic project-level configuration of our modules
-  // responsible for serving static assets, managing page templates and
-  // configuring user accounts.
+	// See lib/modules for basic project-level configuration of our modules
+	// responsible for serving static assets, managing page templates and
+	// configuring user accounts.
 
-  modules: {
-    // Apostrophe module configuration
+	modules: {
+		// Apostrophe module configuration
 
-    // Note: most configuration occurs in the respective
-    // modules' directories. See lib/apostrophe-assets/index.js for an example.
+		// Note: most configuration occurs in the respective
+		// modules' directories. See lib/apostrophe-assets/index.js for an example.
 
-    // However any modules that are not present by default in Apostrophe must at
-    // least have a minimal configuration here: `moduleName: {}`
+		// However any modules that are not present by default in Apostrophe must at
+		// least have a minimal configuration here: `moduleName: {}`
 
-    // If a template is not found somewhere else, serve it from the top-level
-    // `views/` folder of the project
+		// If a template is not found somewhere else, serve it from the top-level
+		// `views/` folder of the project
 
-    "apostrophe-templates": {
-      viewsFolderFallback: path.join(__dirname, "views")
-    },
+		"apostrophe-templates": {
+			viewsFolderFallback: path.join(__dirname, "views")
+		},
 
-    "apostrophe-pages": {
-      types: [
-        {
-          name: "default",
-          label: "Default"
-        },
-        {
-          name: "home",
-          label: "Home"
-        }
-      ]
-    },
+		"apostrophe-pages": {
+			types: [
+				{
+					name: "default",
+					label: "Default"
+				},
+				{
+					name: "home",
+					label: "Home"
+				},
+				{
+					name: "people-page",
+					label: "People"
+				}
+			]
+		},
 
-    // piece types
-    people: {},
-    "people-widgets": {
-      extend: "apostrophe-pieces-widgets"
-    },
-    sandwich: {},
-    poems: { extend: "apostrophe-pieces" },
-    "poems-pages": { extend: "apostrophe-pieces-pages" },
-    "poems-widgets": {},
-    // widgets
-    "fancy-button-widgets": {}
-  }
+		// piece types
+		people: {},
+		"people-pages": {
+			extend: "apostrophe-pieces-pages"
+		},
+		"people-widgets": {
+			extend: "apostrophe-pieces-widgets"
+		},
+		sandwich: {},
+		poems: { extend: "apostrophe-pieces" },
+		"poems-pages": { extend: "apostrophe-pieces-pages" },
+		"poems-widgets": {},
+		// widgets
+		"fancy-button-widgets": {}
+	}
 });
